d0eb0ce04af27e7812b4c92edf2bff1e09fda8d8
HenrySomething03 <henry.bover@my.liu.edu>
2025-04-21 12:20:53 -0400

    Add files via upload

---
+++ MaintenanceSystem.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef MAINTENANCE_SYSTEM_H
+#define MAINTENANCE_SYSTEM_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "Beverage.h"
+
+class MaintenanceSystem {
+private:
+	std::vector<Beverage> beverageCatalog;
+	const std::string accessCode;
+	static const int MAX_DRINK_REFILL = 50;
+	static const int MAX_LABELNAME_LENGTH = 20;
+    const float MIN_COST_VALUE = 0.50f;
+    const float MAX_COST_VALUE = 10.00f;
+
+	std::vector<Beverage> prepareBeverage();
+	bool verifyMaintenanceUser();
+	void ShowInventory();
+	void refillBeverage();
+	void updateBeverageDetails();
+	void showMaintenanceMenu();
+
+public:
+	MaintenanceSystem();
+	void start();
+};
+
+#endif
