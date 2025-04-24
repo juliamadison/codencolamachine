879de18a4d1008264974b7b335d4bdec7311db4c
HenrySomething03 <henry.bover@my.liu.edu>
2025-04-24 17:59:09 -0400

    Update Beverage.h

--- money component- julia/Beverage.h
+++ money component- julia/Beverage.h
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef BEVERAGE_H
-#define BEVERAGE_H
+#ifndef SODA_H
+#define SODA_H
 
 #include <string>
 
@@ -8,9 +8,27 @@ class Beverage {
 public:
     std::string name;
     float price;
+    int slotID;
     int stock;
+    int maxCapacity;
 
-    Beverage(std::string name, float price, int stock) : name(name), price(price), stock(stock) {}
+
+    Beverage(std::string name, float price, int slotID, int stock, int maxCapacity) : name(name), price(price), slotID(slotID), stock(stock), maxCapacity(maxCapacity) {}
 };
+
+void updateStock(int amount) {
+    stock += quantity
+    if (stock > maxCapacity) stock = maxCapacity;
+}
+
+bool isSlotEmpty() const {
+     return stock == 0;
+}
+
+bool isSlotFull() const {
+    return stock >= maxCapacity;
+}
+};
+    
 
 #endif 
